f07217b8da55f65735843a04e2f182cfc1f96842
AZOUANE-Djamila <azouanedjamila@gmail.com>
2021-12-15 06:52:32 +0100

    Commit initial project

---
+++ src/main/java/TP17/azouane_djamila/AzouaneDjamilaApplication.java
@@ -0,0 +1,13 @@
+package TP17.azouane_djamila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzouaneDjamilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzouaneDjamilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TP17/azouane_djamila/AzouaneDjamilaApplicationTests.java
@@ -0,0 +1,13 @@
+package TP17.azouane_djamila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzouaneDjamilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
